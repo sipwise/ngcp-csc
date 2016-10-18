d0c0511343e36107c78ff539ee977dc0c76694d9
Robert Axelsen <raxelsen@sipwise.com>
2016-10-18 14:03:29 +0200

    TT#4306 Add Dockerfile and testrunner script for Jenkins test
    Add initial Dockerfile with Sencha, Selenium and Node.js/NPM specific
    packages. Add testrunner script to start Selenium Server, install needed
    packages for testing and start Karma and WebdriverIO tests.
    
    Change-Id: I9542502a0908fc1b34c01d2b0f792a78e3ba6f79

--- t/karma/karma-setup.spec.js
+++ t/karma/karma-setup.spec.js
@@ -1,4 +1,3 @@
-// Be descriptive with titles here. The describe and it titles combined read like a sentence.
 describe('Karma setup check', function() {
     it('has a dummy spec to check if included files are there', function() {
         console.log(Ext.create('NgcpCsc.model.Call'));
