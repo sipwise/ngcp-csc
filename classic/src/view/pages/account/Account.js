20a2e0666c60aee3850e006d74e1cff7be6efe6b
Robert Axelsen <raxelsen@sipwise.com>
2016-11-08 09:34:37 +0100

    TT#5020 Implement ngcp-csc account module mockup
    Implement ngcp-csc account module mockup, linked with corresponsing
    logged in ACL account. Also remove some console.logs, delete
    unneeded bootstrap.js and bootstrap.css files, and replace main model
    username object with formula object to correctly display username
    in header also immediately after log in.
    
    Change-Id: I91d4b7bfbaf7d27908640b9f730024f63b729695

---
+++ classic/src/view/pages/account/Account.js
@@ -0,0 +1,57 @@
+Ext.define('NgcpCsc.view.pages.account.Account', {
+    extend: 'Ext.panel.Panel',
+
+    xtype: 'account',
+
+    controller: 'account',
+
+    title: Ngcp.csc.locales.account.title[localStorage.getItem('languageSelected')],
+
+    defaults: {
+        padding: 20
+    },
+
+    initComponent: function() {
+        var currentUsername = localStorage.getItem('username');
+        this.items = [{
+            items: [{
+                height: 60,
+                padding: '20 0 5 20',
+                html: Ngcp.csc.locales.account.subtitle[localStorage.getItem('languageSelected')]
+            }, {
+                height: 60,
+                padding: '20 0 5 20',
+                html: Ngcp.csc.locales.account.password_instructions[localStorage.getItem('languageSelected')]
+            }, {
+                xtype: 'accountform'
+            }, {
+                xtype: 'container',
+                height: 40,
+                padding: '10 0 10 10',
+                html: Ngcp.csc.locales.account.change_password[localStorage.getItem('languageSelected')].toLowerCase(),
+                cls: 'link',
+                listeners: {
+                    click: {
+                        element: 'el',
+                        fn: 'submitForm'
+                    }
+                }
+            }, {
+                xtype: 'container',
+                height: 40,
+                padding: '10 0 10 10',
+                html: Ngcp.csc.locales.account.reset_form[localStorage.getItem('languageSelected')].toLowerCase(),
+                cls: 'link',
+                listeners: {
+                    click: {
+                        element: 'el',
+                        fn: 'resetForm'
+                    }
+                }
+            }]
+
+        }];
+        this.callParent();
+    }
+
+});
