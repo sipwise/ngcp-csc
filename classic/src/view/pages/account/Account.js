358d4d06fe4afd394d54d29650218f5eba9efddb
Carlo <cvenusino@sipwise.com>
2016-11-09 14:19:20 +0100

    TT#5745 ngcp-csc account bugfix related to localstorage
    Change-Id: I55147e30bcb7484133eddbdb3817b3f3cc9a67d2

--- classic/src/view/pages/account/Account.js
+++ classic/src/view/pages/account/Account.js
@@ -11,40 +11,43 @@ Ext.define('NgcpCsc.view.pages.account.Account', {
         padding: 20
     },
 
-    items: [{
-        height: 60,
-        padding: '20 0 5 20',
-        html: Ngcp.csc.locales.account.subtitle[localStorage.getItem('languageSelected')]
-    }, {
-        height: 60,
-        padding: '20 0 5 20',
-        html: Ngcp.csc.locales.account.password_instructions[localStorage.getItem('languageSelected')]
-    }, {
-        xtype: 'accountform'
-    }, {
-        xtype: 'container',
-        height: 40,
-        padding: '10 0 10 10',
-        html: Ngcp.csc.locales.account.change_password[localStorage.getItem('languageSelected')].toLowerCase(),
-        cls: 'link',
-        listeners: {
-            click: {
-                element: 'el',
-                fn: 'submitForm'
+    initComponent: function(){
+        this.items = [{
+            height: 60,
+            padding: '20 0 5 20',
+            html: Ngcp.csc.locales.account.subtitle[localStorage.getItem('languageSelected')]
+        }, {
+            height: 60,
+            padding: '20 0 5 20',
+            html: Ngcp.csc.locales.account.password_instructions[localStorage.getItem('languageSelected')]
+        }, {
+            xtype: 'accountform'
+        }, {
+            xtype: 'container',
+            height: 40,
+            padding: '10 0 10 10',
+            html: Ngcp.csc.locales.account.change_password[localStorage.getItem('languageSelected')].toLowerCase(),
+            cls: 'link',
+            listeners: {
+                click: {
+                    element: 'el',
+                    fn: 'submitForm'
+                }
             }
-        }
-    }, {
-        xtype: 'container',
-        height: 40,
-        padding: '10 0 10 10',
-        html: Ngcp.csc.locales.account.reset_form[localStorage.getItem('languageSelected')].toLowerCase(),
-        cls: 'link',
-        listeners: {
-            click: {
-                element: 'el',
-                fn: 'resetForm'
+        }, {
+            xtype: 'container',
+            height: 40,
+            padding: '10 0 10 10',
+            html: Ngcp.csc.locales.account.reset_form[localStorage.getItem('languageSelected')].toLowerCase(),
+            cls: 'link',
+            listeners: {
+                click: {
+                    element: 'el',
+                    fn: 'resetForm'
+                }
             }
-        }
-    }]
+        }];
+        this.callParent();
+    }
 
 });
