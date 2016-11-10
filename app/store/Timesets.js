a3c7f43bce2b5c005a1e84664640b8f28286ba20
Robert Axelsen <raxelsen@sipwise.com>
2016-11-10 11:49:40 +0100

    TT#4312 Implement ngcp-csc call forward module mockup
    Implementing new iteration of cf module, based on new sketches and
    specs from agranig.
    
    Change-Id: Ie1085459f340e437daec39963b3a29f283adbc1c

---
+++ app/store/Timesets.js
@@ -0,0 +1,22 @@
+Ext.define('NgcpCsc.store.Timesets', {
+    extend: 'Ext.data.Store',
+
+    storeId: 'Timesets',
+
+    fields: [
+        {name: 'period', type: 'string'}
+    ],
+
+    data: [
+        {
+            period: 'Always'
+        },{
+            period: 'During office hours'
+        },{
+            period: 'Out of office hours'
+        },{
+            period: '+ New period'
+        }
+    ]
+
+});
