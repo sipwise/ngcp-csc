ef753d5c4a2bb9c3054b1f0b2794f46ed21143ec
Robert Axelsen <raxelsen@sipwise.com>
2016-11-22 13:57:40 +0100

    TT#4314 Implement ngcp-csc fax send module mockup
    > Initial scaffolding
    > Basic styling to increase content textarea height
    > Implement combobox for quality choices, with it's own store
    > Locales values added
    > Create controllers for form submit, reset, and enter keypress
    > Implement textfield tooltips
    
    Change-Id: Ibc136c2678ce1afaa032ccb720a3fcf7701632a7

---
+++ classic/src/view/pages/faxsend/FaxSendController.js
@@ -0,0 +1,37 @@
+Ext.define('NgcpCsc.view.pages.faxsend.FaxSendController', {
+    extend: 'Ext.app.ViewController',
+
+    alias: 'controller.faxsend',
+
+    resetForm: function() {
+        var vm = this.getViewModel();
+        vm.set('destination_number', '');
+        vm.set('page_header', '');
+        vm.set('content', '');
+        vm.set('chosen_file', '');
+        Ext.ComponentQuery.query('#faxsend-quality')[0].reset();
+        Ext.ComponentQuery.query('#fs-file')[0].reset();
+    },
+
+    submitForm: function (field) {
+        // var vm = this.getViewModel();
+        // var newNumberEntered = vm.get('destination_number');
+        // var newHeaderEntered = vm.get('page_header');
+        // var newConteredEntered = vm.get('content');
+        // var newQualitySelected = vm.get('selected_quality');
+        // var newFileChosen = vm.get('chosen_file');
+        this.resetForm();
+    },
+
+    onEnterPressed: function (field, el) {
+        if (el.getKey() == el.ENTER) {
+            this.submitForm(field.id);
+        };
+    },
+
+    selectQuality: function (cmp, rec) {
+        var selectedValue = rec.get('quality');
+        this.getViewModel().set('selected_quality', selectedValue);
+    }
+
+})
---
+++ classic/src/view/pages/faxsend/FaxSendForm.js
@@ -0,0 +1,57 @@
+Ext.define('NgcpCsc.view.pages.faxsend.FaxSendForm', {
+    extend: 'Ext.form.Panel',
+
+    xtype: 'faxsendform',
+    defaultType: 'textfield',
+    layout: 'form',
+    width: 750,
+
+    items: [{
+        fieldLabel: Ngcp.csc.locales.fax_send.field_labels.destination_number[localStorage.getItem('languageSelected')],
+        bind: '{destination_number}',
+        inputAttrTpl: 'data-qtip="' + Ngcp.csc.locales.fax_send.tooltips.number_fax[localStorage.getItem('languageSelected')] + '"',
+        listeners: {
+            specialKey: 'onEnterPressed'
+        }
+    }, {
+        xtype: 'combo',
+        fieldLabel: Ngcp.csc.locales.fax_send.field_labels.quality[localStorage.getItem('languageSelected')],
+        padding: '0 0 0 15',
+        store: 'FaxSendQuality',
+        queryMode: 'local',
+        itemId: 'faxsend-quality',
+        valueField: 'quality',
+        displayField: 'quality',
+        editable: false,
+        value: 'Standard',
+        inputAttrTpl: 'data-qtip="' + Ngcp.csc.locales.fax_send.tooltips.fax_quality[localStorage.getItem('languageSelected')] + '"',
+        listeners: {
+            'select': 'selectQuality'
+        }
+    }, {
+        fieldLabel: Ngcp.csc.locales.fax_send.field_labels.page_header[localStorage.getItem('languageSelected')],
+        bind: '{page_header}',
+        inputAttrTpl: 'data-qtip="' + Ngcp.csc.locales.fax_send.tooltips.header_text[localStorage.getItem('languageSelected')] + '"',
+        listeners: {
+            specialKey: 'onEnterPressed'
+        }
+    }, {
+        xtype: 'textareafield',
+        fieldLabel: Ngcp.csc.locales.fax_send.field_labels.content[localStorage.getItem('languageSelected')],
+        bind: '{content}',
+        cls: 'faxsend-content',
+        inputAttrTpl: 'data-qtip="' + Ngcp.csc.locales.fax_send.tooltips.content_text[localStorage.getItem('languageSelected')] + '"',
+        listeners: {
+            specialKey: 'onEnterPressed'
+        }
+    }, {
+        xtype: 'filefield',
+        itemId: 'fs-file',
+        fieldLabel: Ngcp.csc.locales.fax_send.field_labels.file[localStorage.getItem('languageSelected')],
+        anchor: '100%',
+        inputAttrTpl: 'data-qtip="' + Ngcp.csc.locales.fax_send.tooltips.file_to_send[localStorage.getItem('languageSelected')] + '"',
+        bind: '{file}',
+        buttonText: Ngcp.csc.locales.fax_send.choose_file[localStorage.getItem('languageSelected')]
+    }]
+
+})
