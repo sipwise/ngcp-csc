5e726d09baf3335be9bca5cb5b1e55d258ab52c8
Robert Axelsen <raxelsen@sipwise.com>
2016-12-02 16:21:25 +0100

    TT# Implement consistent locales naming convention
    Change-Id: I55cac0c0492b414eb3d73acfa640f87f079f8557

--- classic/src/view/pages/faxspool/FaxSpool.js
+++ classic/src/view/pages/faxspool/FaxSpool.js
@@ -5,7 +5,7 @@ Ext.define('NgcpCsc.view.pages.faxspool.FaxSpool', {
 
     controller: 'faxspool',
 
-    title: Ngcp.csc.locales.fax_spool.title[localStorage.getItem('languageSelected')],
+    title: Ngcp.csc.locales.faxspool.title[localStorage.getItem('languageSelected')],
 
     layout: 'responsivecolumn',
 
@@ -15,11 +15,11 @@ Ext.define('NgcpCsc.view.pages.faxspool.FaxSpool', {
             padding:30,
             items: [{
                 height: 40,
-                html: Ngcp.csc.locales.fax_spool.subtitle[localStorage.getItem('languageSelected')]
+                html: Ngcp.csc.locales.faxspool.subtitle[localStorage.getItem('languageSelected')]
             }, {
                 height: 80,
                 padding: '5 0 0 0',
-                html: Ext.String.format('<div class="faxspool-heading">{0} {1}</div>', Ngcp.csc.locales.fax_spool.user_label[localStorage.getItem('languageSelected')], localStorage.getItem('username'))
+                html: Ext.String.format('<div class="faxspool-heading">{0} {1}</div>', Ngcp.csc.locales.faxspool.user_label[localStorage.getItem('languageSelected')], localStorage.getItem('username'))
             }, {
                 xtype: 'faxspool-grid'
             }]
