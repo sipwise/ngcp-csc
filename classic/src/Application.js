69a28e047753b663dbc3281540c62d2c95bfc6f3
Carlo <cvenusino@sipwise.com>
2016-12-22 10:30:49 +0100

    TT#5015 ngcp-csc dashboard redesign
    - sample charts added
    - dummy data added
    
    Change-Id: I85a9e6e0ab20298610702e0db63c25e2597b08b6

--- classic/src/Application.js
+++ classic/src/Application.js
@@ -26,7 +26,8 @@ Ext.define('NgcpCsc.Application', {
         'CallBarringIncoming',
         'Addressbook',
         'FaxSpool',
-        'FaxSendQuality'
+        'FaxSendQuality',
+        'Chart'
     ],
 
     launch: function() {
