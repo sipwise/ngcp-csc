2dc32b3898add98d6d9cf5ac4d0153ad7fe4ffaf
Carlo <cvenusino@sipwise.com>
2016-12-28 12:06:17 +0100

    TT#7556 ngcp-csc Subscriber Admin > Seats section
    - module scaffold
    - data layer
    - filter and card layout
    - add / remove seats
    
    Change-Id: I32ed25dcd51f77d6b272efd87d4edb1bdea521ee
    
    TT#4322 ngcp-csc Suscriber admin > Devices section
    
    - scaffold
    
    Change-Id: I23d70c1a668bf986571bef080433b681bd5bf83e

---
+++ classic/src/view/pages/subscriberadmin/devices/DevicesController.js
@@ -0,0 +1,63 @@
+Ext.define('NgcpCsc.view.pages.subscriberadmin.devices.DevicesController', {
+    extend: 'Ext.app.ViewController',
+
+    alias: 'controller.devices',
+
+    onIconClicked: function(event, el) {
+        // eval is never the best option
+        Ext.Function.defer(eval('this.' + el.dataset.callback), 1, this, [el.id]);
+    },
+
+    addDevice: function() {
+        var grid = this.getView().down('devices-grid');
+        var form = this.lookupReference('add-new-device');
+        var store = Ext.getStore('Devices');
+        var newRec = store.insert(0, {
+            id: Ext.id(),
+            expanded: true
+        })[0];
+        grid.getPlugin('rowexpander').toggleRow(0, newRec);
+        grid.getSelectionModel().select(newRec);
+        form.show();
+    },
+
+    editDevice: function(id) {
+        var form = this.lookupReference('add-new-device');
+        var grid = this.getView().down('devices-grid');
+        var store = Ext.getStore('Devices');
+        var selectedRow = store.findRecord('id', id);
+        grid.getSelectionModel().select(selectedRow);
+        form.show();
+    },
+
+    removeDevice: function(id) {
+        var form = this.lookupReference('add-new-device');
+        var store = Ext.getStore('Devices');
+        var selectedRow = store.findRecord('id', id);
+        store.remove(selectedRow);
+        form.hide();
+        this.fireEvent('showmessage', true, Ngcp.csc.locales.common.remove_success[localStorage.getItem('languageSelected')]);
+    },
+
+    resetChanges: function() {
+        var form = this.lookupReference('add-new-device');
+        var grid = this.getView().down('devices-grid');
+        var store = Ext.getStore('Devices');
+        store.rejectChanges();
+        grid.getSelectionModel().deselectAll();
+        form.hide();
+    },
+
+    saveChanges: function() {
+        var form = this.lookupReference('add-new-device');
+        if (!form.isValid()) {
+            return;
+        }
+        var store = Ext.getStore('Devices');
+        var grid = this.getView().down('devices-grid');
+        store.commitChanges();
+        grid.getSelectionModel().deselectAll();
+        this.fireEvent('showmessage', true, Ngcp.csc.locales.common.save_success[localStorage.getItem('languageSelected')]);
+        form.hide();
+    }
+});
---
+++ classic/src/view/pages/subscriberadmin/devices/DevicesGrid.js
@@ -0,0 +1,51 @@
+Ext.define('NgcpCsc.view.pages.devices.DevicesGrid', {
+
+    extend: 'NgcpCsc.view.core.GridCards',
+
+    xtype: 'devices-grid',
+
+    reference: 'devicesGrid',
+
+    store: 'Devices',
+
+    cls: 'card-grid',
+
+    listeners: {
+         click: 'onIconClicked',
+         element: 'el',
+         delegate: 'div.card-icon'
+    },
+
+    rowLines: false,
+
+    viewConfig: {
+        stripeRows: false,
+        columnLines: false
+    },
+
+    columns: {
+        defaults: {
+            menuDisabled: true,
+            resizable: false
+        },
+        items: [{
+            flex: 1,
+            dataIndex: 'name'
+        }]
+    },
+
+    initComponent: function() {
+        this.plugins = [{
+            ptype: 'rowexpander',
+            selectRowOnExpand: false,
+            expandOnDblClick: true,
+            id:'rowexpander',
+            rowBodyTpl: new Ext.XTemplate(
+                '<div class="card-wrapper">',
+                // TODO
+                '</div></div>'
+            )
+        }];
+        this.callParent();
+    }
+})
