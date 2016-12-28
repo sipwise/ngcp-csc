4118fb5e6fe80f02a94aadd4fea334246b9bd099
Carlo <cvenusino@sipwise.com>
2016-12-28 12:06:17 +0100

    TT#7556 ngcp-csc Subscriber Admin > Seats section
    - module scaffold
    - data layer
    - filter and card layout
    - add / remove seats
    
    Change-Id: I32ed25dcd51f77d6b272efd87d4edb1bdea521ee
    
    TT#7556 ngcp-csc Subscriber Admin groups
    
    Change-Id: I7e2202bbfe59bb7186b1cd882c52bfcea25e8107

---
+++ classic/src/view/pages/subscriberadmin/groups/GroupsGrid.js
@@ -0,0 +1,56 @@
+Ext.define('NgcpCsc.view.pages.groups.GroupsGrid', {
+
+    extend: 'NgcpCsc.view.core.GridCards',
+
+    xtype: 'groups-grid',
+
+    reference: 'groupsGrid',
+
+    store: 'Groups',
+
+    cls: 'card-grid',
+
+    listeners: {
+         click: 'onIconClicked',
+         element: 'el',
+         delegate: 'div.card-icon'
+    },
+
+    rowLines: false,
+
+    viewConfig: {
+        stripeRows: false,
+        columnLines: false
+    },
+
+    columns: {
+        defaults: {
+            menuDisabled: true,
+            resizable: false
+        },
+        items: [{
+            flex: 1,
+            dataIndex: 'name'
+        }]
+    },
+
+    initComponent: function() {
+        this.plugins = [{
+            ptype: 'rowexpander',
+            selectRowOnExpand: false,
+            expandOnDblClick: true,
+            id:'rowexpander',
+            rowBodyTpl: new Ext.XTemplate(
+                '<div class="card-wrapper">',
+                '<div class="card-data-row"><span class="fa fa-file-text-o"></span><b>' + Ngcp.csc.locales.filters.extensions[localStorage.getItem('languageSelected')] + '</b>: {extension} </div>',
+                '<div class="card-data-row"><span class="fa fa-file-text-o"></span><b>' + Ngcp.csc.locales.filters.hunt_policy[localStorage.getItem('languageSelected')] + '</b>: {hunt_policy} </div>',
+                '<div class="card-data-row"><span class="fa fa-file-text-o"></span><b>' + Ngcp.csc.locales.filters.hunt_timeout[localStorage.getItem('languageSelected')] + '</b>: {hunt_timeout} </div>',
+                '<div class="card-icon-row">',
+                '<div id="{id}" class="card-icon" data-callback="editGroup"><i class="fa fa-edit green-icon fa-2x pointer" aria-hidden="true"></i></div>',
+                '<div id="{id}" class="card-icon" data-callback="removeGroup"><i class="fa fa-trash green-icon fa-2x pointer" aria-hidden="true"></i></div>',
+                '</div></div>'
+            )
+        }];
+        this.callParent();
+    }
+})
