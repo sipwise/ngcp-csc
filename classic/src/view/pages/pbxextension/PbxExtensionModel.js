55b84e184c78854d20c6f8401d184bd5c41e179e
Robert Axelsen <raxelsen@sipwise.com>
2017-02-01 14:35:55 +0100

    TT#9859 Ngcp-csc remove fax send and pbxext module
    Remove pbx extensions and fax send module. Change auto attendant
    module to sub module of pbx config submodule (awaiting specs from
    Andreas, so leave this module as is for now). Correct naming mistake
    for pbxconfig folder (is named pbfconfig), and remove redundant scss
    and locales.
    
    Leave Ngcp.csc.locales.faxsend locales object for now, as it can be
    reused for the upcoming "Action/calls sidebar" module.
    
    Change-Id: Id951a943dedae24b1719953ba62f38f0b6ae69c0

--- classic/src/view/pages/pbxextension/PbxExtensionModel.js
+++
@@ -1,9 +0,0 @@
-Ext.define('NgcpCsc.view.pages.pbxextension.PbxExtensionModel', {
-    extend: 'Ext.app.ViewModel',
-    alias: 'viewmodel.pbxextension',
-
-    data: {
-        test_viewmodel_data: 'Test data from view model.'
-    }
-
-});
