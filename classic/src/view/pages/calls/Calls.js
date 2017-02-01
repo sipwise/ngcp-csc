693740b7de36cf0e4f97e5bc1ef3a4e45dd97483
Carlo <cvenusino@sipwise.com>
2017-02-01 13:25:55 +0100

    TT#10013 Ngcp-csc card layout adjustments
    - singleclick to expand
    - cosmetic adjustments
    - centered cards in desktop mode
    
    Change-Id: I8462bd92de0cb36b4c7e61d97d1326b127d34fad

--- classic/src/view/pages/calls/Calls.js
+++ classic/src/view/pages/calls/Calls.js
@@ -7,18 +7,10 @@ Ext.define('NgcpCsc.view.pages.calls.Calls', {
 
     viewModel: 'calls',
 
-    layout: 'responsivecolumn',
-
     title: Ngcp.csc.locales.calls.section_title[localStorage.getItem('languageSelected')],
 
     items: [{
-        userCls: 'big-30 small-100',
-        items: [Ext.create('NgcpCsc.view.common.gridfilters.GridFilters', {
-            _linkedStoreId: 'Calls',
-            _callFilters: true
-        })]
-    }, {
         xtype: 'calls-grid',
-        userCls: 'big-70 small-100'
+        userCls: Ext.os.is.Desktop ? 'big-820' : 'small-100'
     }]
-})
+});
