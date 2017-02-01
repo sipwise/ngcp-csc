89ac327353e5f051840519d934a80cbbc4a99caa
Carlo <cvenusino@sipwise.com>
2017-02-01 13:25:55 +0100

    TT#10013 Ngcp-csc card layout adjustments
    - singleclick to expand
    - cosmetic adjustments
    - centered cards in desktop mode
    
    Change-Id: I8462bd92de0cb36b4c7e61d97d1326b127d34fad

--- classic/src/view/core/GridCards.js
+++ classic/src/view/core/GridCards.js
@@ -1,5 +1,18 @@
 Ext.define('NgcpCsc.view.core.GridCards', {
     extend: 'Ext.grid.Panel',
     xtype: 'core-grid-cards',
-    ui: 'core-grid-cards'
+    ui: 'core-grid-cards',
+    listeners: {
+        cellclick: function(view, td, cellIndex, record, tr, rowIndex) {
+            view.grid.getPlugin('rowexpander').toggleRow(rowIndex, record);
+        }
+    },
+    plugins: [{
+        ptype: 'rowwidget'
+            // the widget should be defined in initComponent function
+            // of the views extending this view, via
+            // [view].getPlugin('rowwidget').setWidget({ xtype definition }).
+            // For more info on widget configuration
+            // pls visit http://examples.sencha.com/extjs/6.2.0/examples/kitchensink/#row-widget-grid
+    }]
 });
