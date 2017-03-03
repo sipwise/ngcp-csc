ed9491b5f5a4a6638f6537c217606cd2221a001a
Robert Axelsen <raxelsen@sipwise.com>
2017-03-03 10:43:58 +0100

    TT#11851 Implement CSC pbx inline editing
    Implement inline editing and "add new" functionality for pbx
    submodules; groups, seats and devices.
    
    Due to issues with rowwidget, implement workaround for binding, and
    click listener - which are not working as expected/at all (respctvly).
    
    Also refactor structure of views, to consolidate duplicate code for
    these submodules.
    
    Change-Id: If6b34fff1634e87aedb28232a859529e7616173d

---
+++ classic/src/view/pages/pbxconfig/PbxConfigModel.js
@@ -0,0 +1,9 @@
+Ext.define('NgcpCsc.view.pages.pbxconfig.PbxConfigModel', {
+    extend: 'Ext.app.ViewModel',
+    alias: 'viewmodel.pbxconfig',
+
+    data: {
+        edit_in_progress: false
+    }
+
+});
