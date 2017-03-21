ec644e56a6f687d49a092490a96cd197864353cf
Robert Axelsen <raxelsen@sipwise.com>
2017-03-21 10:30:10 +0100

    TT#12778 Csc split up CF module, and impl changes
    Split Ngcp-csc call forward module into three submodules based on
    timesource (always/after hours/company hours).
    
    Plus implement the following changes:
    > not bold text
    > new placement of timeset grid, include a "for calls during ___"
      text, and toggle with calendar icon
    > add cursor pointer to sourceset/timeset grid
    > implement tabs based on sourcsets, with clickable icons in tab
    > place sourceset grid topmost in tab panel, and toggle with icon
    > autofocus on bottom list A/B row when add new
    > remove bulletpoints
    > ability to change List A/List B name
    
    Change-Id: Ied00dd73f2044c0af2fb82a0be17c66c3c597663

--- classic/src/view/pages/callforward/CallForward.js
+++ classic/src/view/pages/callforward/CallForward.js
@@ -9,6 +9,22 @@ Ext.define('NgcpCsc.view.pages.callforward.CallForward', {
 
     layout: 'responsivecolumn',
 
+    // DONE: 1. Change from bold to normal font weight
+    // TODO: 2. Remove bullets from lists and emptyText renderer
+    // TODO: 3. Add cursor pointer to sourceset/timeset grids
+    // TODO: 4. Autofocus on bottom list A/B row when add new
+    // TODO: 5. Adjust to NgcpCsc.view.core.GridCards and card-grid layout
+    // TODO: 6. Split into submodules that with segmented buttons for now
+    // TODO: 7. Implement timeset text and icon ("For calls during always ...")
+    // TODO: 8. Place timeset grid to right of timeset text and icon, and toggle
+    // visibility when icon clicked
+    // TODO: 9. In preparation for tabs, plan out how to map the data (more
+    // stores?)
+    // TODO: 10. Implement tabs
+    // TODO: 11. Implement ability to change List A/List B name
+    // TODO: 12. Implement clickable icons in tabs
+    // TODO: 13. Place sourceset grid topmost in tab panel, and toggle with icon
+
     initComponent: function () {
 
         this.items = [{
---
+++ classic/src/view/pages/callforward/companyhours/CompanyHours.js
@@ -0,0 +1,6 @@
+Ext.define('NgcpCsc.view.pages.callforward.companyhours.Companyhours', {
+    extend: 'NgcpCsc.view.pages.callforward.CallForward',
+
+    xtype: 'companyhours'
+
+});
