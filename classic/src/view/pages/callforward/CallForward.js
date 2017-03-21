2b29d5e54a575f99c29ed3eed9e7d5ff94a5a863
Robert Axelsen <raxelsen@sipwise.com>
2017-03-21 10:30:10 +0100

    TT#12778 Csc split up CF module, and impl changes
    Split Ngcp-csc call forward module into three submodules based on
    timesource (always/after hours/company hours).
    
    Plus implement the following changes:
    > not bold text
    > new placement of timeset grid, include a "for calls during ___"
      text, and toggle with calendar icon
    > add cursor pointer to sourceset/timeset grid
    > implement tabs based on sourcsets, with clickable icons in tab
    > place sourceset grid topmost in tab panel, and toggle with icon
    > autofocus on bottom list A/B row when add new
    > remove bulletpoints
    > ability to change List A/List B name
    
    Change-Id: Ied00dd73f2044c0af2fb82a0be17c66c3c597663

--- classic/src/view/pages/callforward/CallForward.js
+++ classic/src/view/pages/callforward/CallForward.js
@@ -9,6 +9,21 @@ Ext.define('NgcpCsc.view.pages.callforward.CallForward', {
 
     layout: 'responsivecolumn',
 
+    // TODO: 1. Change from bold to normal font weight
+    // TODO: 2. Remove bullets from lists and emptyText renderer
+    // TODO: 3. Add cursor pointer to sourceset/timeset grids
+    // TODO: 4. Autofocus on bottom list A/B row when add new
+    // TODO: 5. Split into submodules that with segmented buttons for now
+    // TODO: 6. Implement timeset text and icon ("For calls during always ...")
+    // TODO: 7. Place timeset grid to right of timeset text and icon, and toggle
+    // visibility when icon clicked
+    // TODO: 8. In preparation for tabs, plan out how to map the data (more
+    // stores?)
+    // TODO: 9. Implement tabs
+    // TODO: 10. Implement ability to change List A/List B name
+    // TODO: 11. Implement clickable icons in tabs
+    // TODO: 12. Place sourceset grid topmost in tab panel, and toggle with icon
+
     initComponent: function () {
 
         this.items = [{
---
+++ classic/src/view/pages/callforward/always/Always.js
@@ -0,0 +1,6 @@
+Ext.define('NgcpCsc.view.pages.callforward.always.Always', {
+    extend: 'NgcpCsc.view.pages.callforward.CallForward',
+
+    xtype: 'always'
+
+});
