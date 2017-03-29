933d8ed0e279ffa4c8f3f7c1ce51873d643c893a
Robert Axelsen <raxelsen@sipwise.com>
2017-03-29 14:56:52 +0200

    TT#13650 Csc pbx seats groups adds and changes
    As specified in meeting 20.03, we need to implement new fields and
    tag fields. Need to implement both in rowwidget in view, manual bind
    in controller, add in gridfilters form, and add controller logic to
    handle tag fields both in pbxconfig and gridfilters.
    
    TODOS:
    0. Write todos for whole task
    	1. Add all new view changes
    	a. comboboxes
    	b. tagfields
    	c. remove unneeded fields/labels
    	d. alter existing fields/labels that need changed
    	e. implement timeout field and extra labels for hunt policy
    	f. implement inline XTemplate/tpl for displaying tagfield
    	data as comma-separated text
    2. Implement controller logic for the new view changes
    	a. implement hide/show logic for hunt policy/timeout changes
    	b. implement show/hide logic for rest of new fields/labels
    	c. adjust validation controller
    	d. adjust any other affected controllers
    3. Adjust gridfilters to work with new and adjusted fields
    	a. create new controller logic for also filter on children
    	nodes
    	introduced by groups/aliasnumbers/etc tagfields
    b. implement gridfilter search fields for new fields
    	c. adjust logic for rest of new fields
    4. Cleanup and check for missed locales/icons/comments
    
    Change-Id: I7dad57af0ee6fa40bad1cb839d2d9c17a127211f

--- classic/src/view/pages/pbxconfig/PbxConfig.js
+++ classic/src/view/pages/pbxconfig/PbxConfig.js
@@ -5,6 +5,27 @@ Ext.define('NgcpCsc.view.pages.pbxconfig.PbxConfig', {
     viewModel: 'pbxconfig',
     controller: 'pbxconfig',
 
+    // TODO: 0. Write todos for whole task
+    // TODO: 1. Add all new view changes
+    // TODO:    a. comboboxes
+    // TODO:    b. tagfields
+    // TODO:    c. remove unneeded fields/labels
+    // TODO:    d. alter existing fields/labels that need changed
+    // TODO:    e. implement timeout field and extra labels for hunt policy       
+    // TODO:    f. implement inline XTemplate/tpl for displaying tagfield
+    //             data as comma-separated text
+    // TODO: 2. Implement controller logic for the new view changes
+    // TODO:    a. implement hide/show logic for hunt policy/timeout changes
+    // TODO:    b. implement show/hide logic for rest of new fields/labels
+    // TODO:    c. adjust validation controller
+    // TODO     d. adjust any other affected controllers
+    // TODO: 3. Adjust gridfilters to work with new and adjusted fields
+    // TODO:    a. create new controller logic for also filter on children nodes
+    //             introduced by groups/aliasnumbers/etc tagfields
+    // TODO:    b. implement gridfilter search fields for new fields
+    // TODO:    c. adjust logic for rest of new fields
+    // TODO: 4. Cleanup and check for missed locales/icons/comments
+
     initComponent: function() {
 
         this.dockedItems = [{
