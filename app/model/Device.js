3ed61b0110154d12126bb148057ac9787d75d5c9
Carlo <cvenusino@sipwise.com>
2017-03-31 15:16:47 +0200

    TT#11852 ngcp-csc Pbx Devices
    Change-Id: I12d7cec5f238c78ca6f300bbc017c3d31e5f3e6d

--- app/model/Device.js
+++ app/model/Device.js
@@ -7,10 +7,10 @@ Ext.define('NgcpCsc.model.Device', {
     }, {
         name: 'device',
         type: 'string'
-    },{
+    }, {
         name: 'mac',
         type: 'string'
-    },{
+    }, {
         name: 'status',
         type: 'string'
     }, {
@@ -19,5 +19,69 @@ Ext.define('NgcpCsc.model.Device', {
     }, {
         name: 'destinations',
         type: 'auto'
+    }, {
+        name: 'imageWithButtons',
+        depends: ['image'],
+        convert: function(v, rec) {
+            var retVal = "<div><img width=550 height=550 src='" + rec.get('image') + "'></div>";
+            var multiply = 1;
+            Ext.each(rec.get('destinations'), function(destination) {
+                if (destination.name) {
+                    var buttonCls = 'assigned-button';
+                    var btnPos = "top:" + destination.position.top + ";left:" + destination.position.left;
+                    var rectPos,
+                        top = destination.position.top,
+                        left = destination.position.left,
+                        lineWidth = (multiply > 1) ? "225px" : "120px",
+                        lineHeight = (multiply > 1) ? "90px" : "50px",
+                        lineTop = top,
+                        lineLeft = destination.position.left,
+                        lineCls = destination.position.anchor == 'left' || destination.position.anchor == 'right' ? 'connection-left-right' : 'connection-top-bottom';
+                    switch (destination.position.anchor) {
+                        case "left":
+                            lineHeight = "1px";
+                            lineWidth = (multiply > 1) ? "200px" : "100px",
+                                left = (parseInt(destination.position.left.split('%')[0]) + (15 * multiply)).toString() + "%";
+                            top = (parseInt(destination.position.top.split('%')[0]) - 1).toString() + "%";
+                            break;
+                        case "right":
+                            left = (parseInt(destination.position.left.split('%')[0]) - (20 * multiply)).toString() + "%";
+                            top = (parseInt(destination.position.top.split('%')[0]) - 1).toString() + "%";
+                            lineLeft = left;
+                            lineHeight = "1px";
+                            break;
+                        case "top":
+                            lineLeft = (parseInt(destination.position.left.split('%')[0]) + 1).toString() + "%";
+                            top = (parseInt(destination.position.top.split('%')[0]) + (7 * multiply)).toString() + "%";
+                            break;
+                        case "bottom":
+                            lineLeft = (parseInt(destination.position.left.split('%')[0]) + 1).toString() + "%";
+                            top = (parseInt(destination.position.top.split('%')[0]) - (7 * multiply)).toString() + "%";
+                            break;
+                    }
+
+                    retVal += '<div><span data-callback="btnHovered" data-onclick="seatClick" class="button-info pointer card-icon" style="top:' +
+                        top + ';left:' + left + ';" id="destination-info' +
+                        rec.get('id') + "-" + destination.order + '">' +
+                        destination.order + ' | ' + destination.name + '</span><span class="' +
+                        lineCls + ' pointer" style="height:' + lineHeight + ';width:' +
+                        lineWidth + ';top:' + lineTop + ';left:' +
+                        lineLeft + ';"></span><span id="destination-"' +
+                        rec.get('id') + "-" + destination.order + '" class="' +
+                        buttonCls + '" style="' +
+                        btnPos + '"></span></div>';
+                } else {
+                    var top = (parseInt(destination.position.top.split('%')[0]) - 1).toString() + "%";
+                    var btnPos = "top:" + top + ";left:" + destination.position.left;
+                    var buttonCls = 'free-button';
+                    retVal += '<div data-qtip="Click to assign" data-callback="btnHovered" data-onclick="seatClick" id="destination-' +rec.get('id') + '-' + destination.order + '" class="' +
+                        buttonCls + ' pointer card-icon" style="' +
+                        btnPos + '"></div>';
+
+                }
+                multiply == 1 ? multiply++ : multiply--;
+            });
+            return retVal;
+        }
     }]
 });
--- classic/src/view/pages/pbxconfig/devices/DevicesGrid.js
+++ classic/src/view/pages/pbxconfig/devices/DevicesGrid.js
@@ -6,6 +6,8 @@ Ext.define('NgcpCsc.view.pages.pbxconfig.devices.DevicesGrid', {
     reference: 'devicesGrid',
     store: 'Devices',
 
+    viewModel: 'devices',
+
     viewConfig: {
         stripeRows: false,
         enableTextSelection: true
@@ -15,7 +17,12 @@ Ext.define('NgcpCsc.view.pages.pbxconfig.devices.DevicesGrid', {
         click: {
             fn: 'onIconClicked',
             element: 'el',
-            delegate: 'div.card-icon'
+            delegate: '.card-icon'
+        },
+        mouseenter: {
+            fn: 'onMouseEntered',
+            element: 'el',
+            delegate: '.card-icon'
         },
         cellclick: 'expandRow',
         rowbodyclick: 'expandRow'
@@ -39,6 +46,7 @@ Ext.define('NgcpCsc.view.pages.pbxconfig.devices.DevicesGrid', {
         pluginId: 'rowwidgetDevices',
         ptype: 'rowwidget',
         widget: {
+            cls: 'devices',
             xtype: 'form',
             defaultBindProperty: 'hidden',
             bind: {
@@ -49,142 +57,280 @@ Ext.define('NgcpCsc.view.pages.pbxconfig.devices.DevicesGrid', {
                 layout: 'hbox'
             },
             items: [{
-                name: 'name',
-                defaults: {
-                    padding: '0 0 15 0'
-                },
-                items: [{
-                    xtype: 'label',
-                    bind: {
-                        id: 'devices-label-mainname-{record.id}'
+                    name: 'name',
+                    defaults: {
+                        padding: '0 0 15 0'
                     },
-                    hidden: true,
-                    cls: 'pbx-data-value',
-                    text: Ngcp.csc.locales.pbxconfig.name[localStorage.getItem('languageSelected')],
-                    width: 120
-                }, {
-                    xtype: 'textfield',
-                    required: true,
-                    hidden: true,
-                    emptyText: Ngcp.csc.locales.pbxconfig.enter_new_name[localStorage.getItem('languageSelected')],
-                    bind: {
-                        id: 'devices-textfield-name-{record.id}'
-                    },
-                    listeners: {
-                        // Workaround. Issue when binding is used, any change in any record field triggers a
-                        // layout break in the row which looks like row collapse, but is not
-                        focus: {
-                            fn: 'setFieldValue'
+                    items: [{
+                        xtype: 'label',
+                        bind: {
+                            id: 'devices-label-mainname-{record.id}'
                         },
-                        specialkey: 'onEnterPressed'
-                    }
-                }]
-            }, {
-                name: 'device',
-                defaults: {
-                    padding: '0 0 15 0'
-                },
-                items: [{
-                    xtype: 'label',
-                    cls: 'pbx-data-value',
-                    text: Ngcp.csc.locales.pbxconfig.device[localStorage.getItem('languageSelected')],
-                    width: 120
-                }, {
-                    xtype: 'label',
-                    hidden: false,
-                    bind: {
-                        id: 'devices-label-device-{record.id}',
-                        text: '{record.device}'
-                    }
-                }, {
-                    xtype: 'textfield',
-                    required: true,
-                    hidden: true,
-                    emptyText: Ngcp.csc.locales.pbxconfig.enter_new_device[localStorage.getItem('languageSelected')],
-                    bind: {
-                        id: 'devices-textfield-device-{record.id}'
-                    },
-                    listeners: {
-                        focus: {
-                            fn: 'setFieldValue'
+                        hidden: true,
+                        cls: 'pbx-data-value',
+                        text: Ngcp.csc.locales.pbxconfig.station_name[localStorage.getItem('languageSelected')],
+                        width: 120
+                    }, {
+                        xtype: 'textfield',
+                        required: true,
+                        hidden: true,
+                        emptyText: Ngcp.csc.locales.pbxconfig.enter_new_name[localStorage.getItem('languageSelected')],
+                        bind: {
+                            id: 'devices-textfield-name-{record.id}'
                         },
-                        specialkey: 'onEnterPressed'
-                    }
-                }]
-            }, {
-                name: 'mac',
-                defaults: {
-                    padding: '0 0 15 0'
-                },
-                items: [{
-                    xtype: 'label',
-                    cls: 'pbx-data-value',
-                    text: Ngcp.csc.locales.pbxconfig.mac[localStorage.getItem('languageSelected')],
-                    width: 120
+                        listeners: {
+                            // Workaround. Issue when binding is used, any change in any record field triggers a
+                            // layout break in the row which looks like row collapse, but is not
+                            focus: {
+                                fn: 'setFieldValue'
+                            },
+                            specialkey: 'onEnterPressed'
+                        }
+                    }]
                 }, {
-                    xtype: 'label',
-                    hidden: false,
-                    bind: {
-                        id: 'devices-label-mac-{record.id}',
-                        text: '{record.mac}'
-                    }
-                }, {
-                    xtype: 'textfield',
-                    required: true,
-                    hidden: true,
-                    emptyText: Ngcp.csc.locales.pbxconfig.enter_new_mac_address[localStorage.getItem('languageSelected')],
-                    bind: {
-                        id: 'devices-textfield-mac-{record.id}'
+                    name: 'mac',
+                    defaults: {
+                        padding: '0 0 15 0'
                     },
-                    listeners: {
-                        focus: {
-                            fn: 'setFieldValue'
+                    items: [{
+                        xtype: 'label',
+                        cls: 'pbx-data-value',
+                        text: Ngcp.csc.locales.pbxconfig.mac[localStorage.getItem('languageSelected')],
+                        width: 120
+                    }, {
+                        xtype: 'label',
+                        hidden: false,
+                        bind: {
+                            id: 'devices-label-mac-{record.id}',
+                            text: '{record.mac}'
+                        }
+                    }, {
+                        xtype: 'textfield',
+                        required: true,
+                        hidden: true,
+                        emptyText: Ngcp.csc.locales.pbxconfig.enter_new_mac_address[localStorage.getItem('languageSelected')],
+                        bind: {
+                            id: 'devices-textfield-mac-{record.id}'
                         },
-                        specialkey: 'onEnterPressed'
-                    }
-                }]
-            }, {
-                name: 'status',
-                defaults: {
-                    padding: '0 0 15 0'
-                },
-                items: [{
-                    xtype: 'label',
-                    cls: 'pbx-data-value',
-                    text: Ngcp.csc.locales.pbxconfig.status[localStorage.getItem('languageSelected')],
-                    width: 120
+                        listeners: {
+                            focus: {
+                                fn: 'setFieldValue'
+                            },
+                            specialkey: 'onEnterPressed'
+                        }
+                    }]
                 }, {
-                    xtype: 'label',
-                    hidden: false,
-                    bind: {
-                        id: 'devices-label-status-{record.id}',
-                        text: '{record.status}'
-                    }
-                }, {
-                    xtype: 'textfield',
-                    required: true,
-                    hidden: true,
-                    emptyText: Ngcp.csc.locales.pbxconfig.enter_new_status[localStorage.getItem('languageSelected')],
-                    bind: {
-                        id: 'devices-textfield-status-{record.id}'
+                    name: 'device',
+                    defaults: {
+                        padding: '0 0 15 0'
                     },
-                    listeners: {
-                        focus: {
-                            fn: 'setFieldValue'
+                    items: [{
+                        xtype: 'label',
+                        cls: 'pbx-data-value',
+                        text: Ngcp.csc.locales.pbxconfig.device[localStorage.getItem('languageSelected')],
+                        width: 120
+                    }, {
+                        xtype: 'label',
+                        hidden: false,
+                        bind: {
+                            id: 'devices-label-device-{record.id}',
+                            text: '{record.device}'
+                        }
+                    }, {
+                        xtype: 'combo',
+                        required: true,
+                        autoSelect: false,
+                        hidden: true,
+                        emptyText: Ngcp.csc.locales.pbxconfig.enter_new_device[localStorage.getItem('languageSelected')],
+                        bind: {
+                            id: 'devices-textfield-device-{record.id}'
                         },
-                        specialkey: 'onEnterPressed'
+                        listeners: {
+                            focus: 'setFieldValue',
+                            specialkey: 'onEnterPressed',
+                            select: 'deviceSelected'
+                        },
+                        store: ['Cisco Pbx 1', 'Cisco Pbx 2', 'Cisco Pbx 3']
+                    }]
+                }, {
+                    name: 'extension',
+                    defaults: {
+                        padding: '0 0 15 0'
+                    },
+                    items: [{
+                        xtype: 'label',
+                        cls: 'pbx-data-value',
+                        text: Ngcp.csc.locales.pbxconfig.extension[localStorage.getItem('languageSelected')] + " 1:",
+                        width: 120
+                    }, {
+                        xtype: 'label',
+                        hidden: false,
+                        bind: {
+                            id: 'devices-label-extension-{record.id}',
+                            text: '{record.extension}'
+                        }
+                    }, {
+                        xtype: 'combo',
+                        required: true,
+                        hidden: true,
+                        emptyText: Ngcp.csc.locales.pbxconfig.enter_new_extension[localStorage.getItem('languageSelected')],
+                        bind: {
+                            id: 'devices-textfield-extension-{record.id}'
+                        },
+                        listeners: {
+                            focus: {
+                                fn: 'setFieldValue'
+                            },
+                            specialkey: 'onEnterPressed'
+                        },
+                        store: ['Ext1', 'Ext2', 'Ext3']
+                    }]
+                }, {
+                    name: 'extension2',
+                    defaults: {
+                        padding: '0 0 15 0'
+                    },
+                    items: [{
+                        xtype: 'label',
+                        cls: 'pbx-data-value',
+                        text: Ngcp.csc.locales.pbxconfig.extension[localStorage.getItem('languageSelected')] + " 2:",
+                        width: 120
+                    }, {
+                        xtype: 'label',
+                        hidden: false,
+                        bind: {
+                            id: 'devices-label-extension2-{record.id}',
+                            text: '{record.extension2}'
+                        }
+                    }, {
+                        xtype: 'combo',
+                        required: true,
+                        hidden: true,
+                        emptyText: Ngcp.csc.locales.pbxconfig.enter_new_extension[localStorage.getItem('languageSelected')],
+                        bind: {
+                            id: 'devices-textfield-extension2-{record.id}'
+                        },
+                        listeners: {
+                            focus: {
+                                fn: 'setFieldValue'
+                            },
+                            specialkey: 'onEnterPressed'
+                        },
+                        store: ['Ext1', 'Ext2', 'Ext3']
+                    }]
+                },
+
+
+                {
+                    xtype: 'panel',
+                    layout: {
+                        type: 'hbox'
+                    },
+                    defaults: {
+                        margin: 20
+                    },
+                    items: [{
+                        cls: 'device-img',
+                        xtype: 'label',
+                        width: 550,
+                        height: 550,
+                        bind: {
+                            html: '{record.imageWithButtons}',
+                            hidden: '{!record.image}'
+                        },
+                        listeners: {
+                            render: 'onImgRendered'
+                        }
+                    }, {
+                        xtype: 'fieldset',
+                        width: 150,
+                        collapsible: false,
+                        hidden: true,
+                        defaults: {
+                            xtype:'label',
+                            labelWidth: 50,
+                            anchor: '100%',
+                        },
+                        bind:{
+                            id: 'seat-show-panel-{record.id}'
+                        },
+                        items: [ {
+                            name: 'typeValue'
+                        },{
+                            xtype: 'buttongroup',
+                            name: 'editBtns',
+                            columns: 3,
+                            hidden: true,
+                            cls: 'edit-panel',
+                            border: 0,
+                            defaults:{
+                                width: 50
+                            },
+                            items: [{
+                                iconCls: Ngcp.csc.icons.edit,
+                                handler: 'editSeat'
+                            },
+                            {
+                                iconCls: Ngcp.csc.icons.trash,
+                                handler: 'deleteSeat',
+                                name: 'deleteBtn'
+                            }]
+                        }
+                    ]
+                    },{
+                        xtype: 'fieldset',
+                        width: 150,
+                        collapsible: false,
+                        hidden: true,
+                        defaults: {
+                            labelWidth: 40,
+                            anchor: '100%',
+                        },
+                        bind:{
+                            id: 'seat-edit-panel-{record.id}'
+                        },
+                        items: [{
+                            xtype: 'combo',
+                            name: 'typeValue',
+                            fieldLabel: 'Type',
+                            store: ['Shared', 'Speed dial', 'Busy lamp', 'Private'],
+                            allowBlank: false
+                        }, {
+                            xtype: 'combo',
+                            store: ['User1', 'User2', 'User3', 'User4'],
+                            name: 'seat',
+                            fieldLabel: 'User',
+                            allowBlank: false
+                        },{
+                            xtype: 'buttongroup',
+                            name: 'editBtns',
+                            columns: 3,
+                            cls: 'edit-panel',
+                            border: 0,
+                            defaults:{
+                                width: 50
+                            },
+                            items: [{
+                                iconCls: Ngcp.csc.icons.floppy,
+                                handler: 'saveSeat'
+                            },
+                            {
+                                iconCls: Ngcp.csc.icons.block,
+                                handler: 'discardChanges'
+                            }]
+                        }]
+                    }]
+                }, {
+                    xtype: 'label',
+                    bind: {
+                        html: '<div class="card-wrapper"><div class="card-icon-row">' +
+                            '<div id="removeDevice-{record.id}" class="card-icon" data-callback="removeCard" data-qtip="' + Ngcp.csc.locales.filters.tooltips.remove_seat[localStorage.getItem('languageSelected')] + '"><i class="' + Ngcp.csc.icons.trash2x + ' green-icon pointer" aria-hidden="true"></i></div>' +
+                            '<div id="editDevice-{record.id}" class="card-icon" data-callback="editCard" data-qtip="' + Ngcp.csc.locales.filters.tooltips.edit_seat[localStorage.getItem('languageSelected')] + '"><i class="' + Ngcp.csc.icons.edit2x + ' green-icon pointer" aria-hidden="true"></i></div>' +
+                            '<div id="cancelDevice-{record.id}" class="card-icon hidden" data-callback="cancelCard" data-qtip="' + Ngcp.csc.locales.pbxconfig.cancel_operation[localStorage.getItem('languageSelected')] + '"><i class="' + Ngcp.csc.icons.block2x + ' green-icon pointer" aria-hidden="true"></i></div>' +
+                            '</div></div>'
                     }
-                }]
-            }, {
-                xtype: 'label',
-                bind: {
-                    html: '<div class="card-wrapper"><div class="card-icon-row">' +
-                    '<div id="removeDevice-{record.id}" class="card-icon" data-callback="removeCard" data-qtip="' + Ngcp.csc.locales.filters.tooltips.remove_seat[localStorage.getItem('languageSelected')] + '"><i class="' + Ngcp.csc.icons.trash2x + ' green-icon pointer" aria-hidden="true"></i></div>' +
-                    '<div id="editDevice-{record.id}" class="card-icon" data-callback="editCard" data-qtip="' + Ngcp.csc.locales.filters.tooltips.edit_seat[localStorage.getItem('languageSelected')] + '"><i class="' + Ngcp.csc.icons.edit2x + ' green-icon pointer" aria-hidden="true"></i></div>' +
-                    '<div id="cancelDevice-{record.id}" class="card-icon hidden" data-callback="cancelCard" data-qtip="' + Ngcp.csc.locales.pbxconfig.cancel_operation[localStorage.getItem('languageSelected')] + '"><i class="' + Ngcp.csc.icons.block2x + ' green-icon pointer" aria-hidden="true"></i></div>' +
-                    '</div></div>'
                 }
-            }]
+            ]
         }
     }]
 });
