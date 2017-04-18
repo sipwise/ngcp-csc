88bf740dd01290675e346085fa6b5b733f99f27e
Robert Axelsen <raxelsen@sipwise.com>
2017-04-18 19:32:16 +0200

    TT#14101 Csc Call Forward new changes + additions
    What was done:
     1. Change timeset grid column title from "Closed" to "Full Day"
     2. Implement "save" and "close" buttons for timeset grid, and
        controller (controllers fire msg for now)
     3. Copy "For calls during after hours..." to panel title (keep it
        in both places)
     4. Implement changes to tabs and timeset appearance, see wireframe
        in balsamiq
     5. Change "Sourceset title" to "Title"
     6. Implement Sourceset grid save button in row, and editor field
        slightly less wide to make room for button
     7. Make sourceset title input field full width
     8. Change color for "move icon" to same color as "trash icon"
     9. Implement tooltips in grids
     10. Fix titles to "Call Forward - Always", and remove "For calls
         during always..." in always submodule
     11. Move the loadingBar
     12. Local storage for persistent state of collapsable panel
     13. Full width timeset grid, and place inside panel so it can be
         properly collapsed
     14. Locales all the things
     15. Adjust width of sourceset grid to 100% as well, and move buttons
         to right align
    
    Change-Id: Id31fe3f34cb0009fa24068acc242daaa2b4bb481

--- classic/src/view/pages/callforward/companyhours/CompanyHours.js
+++ classic/src/view/pages/callforward/companyhours/CompanyHours.js
@@ -5,7 +5,7 @@ Ext.define('NgcpCsc.view.pages.callforward.companyhours.Companyhours', {
 
     ui: 'cf-mainform',
 
-    initComponent: function () {
+    initComponent: function() {
         var callForwardCompanyGrid = Ext.create('NgcpCsc.view.pages.callforward.CallForwardTimesetGrid', {
             id: 'cf-timeset-company-grid',
             store: Ext.create('NgcpCsc.store.CallForwardTimeset', {
@@ -24,45 +24,45 @@ Ext.define('NgcpCsc.view.pages.callforward.companyhours.Companyhours', {
             userCls: Ext.os.is.Desktop ? 'big-820' : 'small-100',
             xtype: 'core-container',
             items: [{
-                    layout: 'responsivecolumn',
-                    xtype: 'container',
-                    items: [{
-                        xtype: 'panel',
-                        userCls: 'big-33 small-100 cf-calls-curing-section',
-                        items: [{
-                            layout: 'hbox',
-                            items: [{
-                                xtype: 'container',
-                                userCls: 'cf-calls-during',
-                                html: Ngcp.csc.locales.callforward.for_calls_during_company_hours[localStorage.getItem('languageSelected')],
-                                margin: '10 0 0 0'
-                            }, {
-                                xtype: 'button',
-                                userCls: 'cf-calendar-button',
-                                iconCls: Ngcp.csc.icons.calendar2x,
-                                handler: 'toggleCompanyTimesetGrid'
-                            }]
-                        }]
-                    }, {
-                        xtype: 'panel',
-                        userCls: 'big-66 small-100',
-                        bind: {
-                            hidden: '{company_hours}'
-                        },
-                        items: [
-                            callForwardCompanyGrid
-                        ]
-                    }]
-                },
-                {
-                    xtype: 'container',
-                    userCls: 'cf-text',
-                    html: Ngcp.csc.locales.callforward.for_calling_parties[localStorage.getItem('languageSelected')],
-                    margin: '10 0 10 0'
-                }, {
-                    xtype: 'statusbar',
-                    reference: 'loadingBar'
-                }, {
+                layout: 'responsivecolumn',
+                xtype: 'container',
+                items: [{
+                    xtype: 'panel',
+                    id: 'collapsePanel-companyHours',
+                    title: Ngcp.csc.locales.callforward.for_calls_during_after_hours[localStorage.getItem('languageSelected')],
+                    width: '100%',
+                    collapsible: true,
+                    bind: {
+                        collapsed: '{companyPanelIsCollapsed}'
+                    },
+                    listeners: {
+                        collapse: 'collapsePanel',
+                        expand: 'collapsePanel'
+                    },
+                    userCls: 'big-33 small-100 cf-calls-during-section',
+                    items: [
+                        callForwardCompanyGrid,
+                        {
+                            text: Ngcp.csc.locales.common.save_caps[localStorage.getItem('languageSelected')],
+                            xtype: 'button',
+                            cls: 'x-btn-left',
+                            id: 'companyHours-saveButton',
+                            width: 135,
+                            margin: '10 0 0 585',
+                            listeners: {
+                                click: 'saveGrid'
+                            }
+                        }
+                    ]
+                }]
+            }, {
+                xtype: 'statusbar',
+                reference: 'loadingBar'
+            }, {
+                xtype: 'panel',
+                width: '100%',
+                title: Ngcp.csc.locales.callforward.for_calling_parties[localStorage.getItem('languageSelected')]
+            }, {
                 xtype: 'companyhourstabs'
             }]
         }];
