1e649f2a3039b65a7482689ef243f36759fd1618
Robert Axelsen <raxelsen@sipwise.com>
2017-04-24 17:15:05 +0200

    TT#14851 Csc pbx consolidation, fixed, validation
    What's done:
     1. Reorder menu to groups, seats, devices order
     2. Map out the data flow, see workfront docs
     3. Plan data store merge implementation
     4. Plan out what needs validation, and is required
     5. Implement validation in view and controller
     8. Implement store merge as per Cvenusinos suggestion
        a. introduce in Groups data/model a unique id field (was already
           there)
        b. change seat data to have the groups field as array of group ids
        c. where you assign the value of groups label in seat card, you
           have to iterate&find the records in Groups store which have the
           ids of groups field in seat record, and extract the names
        d. as for the tagfield, you have to use Groups store
        e. define valueField and displayField using fields which exists in
           group model (id & name for example)
        f. if the store has autoLoad: true (=dropdown populated),
           binding the groups field in seat model to the array of ids
           should automatically display the selected group names
        g. Filter out field arrays where fieldLabel is "User" or "Type"
    
    What's remaining:
     6. Bottom of card gets cut off in seats when creating new, might
        also be happening in other modules. Investigate and fix
     7. Fix bug in Groups module, where hunt 'for' label does not get
        extra margin-top when saved with enter, only with button
     9. Fix bugs with "delayed" save, or "first save not working when
        adding new seat or group"
    
    Change-Id: I2af41a6e9799b7cbd268b1217ed49b9381272d6a

--- classic/src/view/pages/pbxconfig/PbxConfig.js
+++ classic/src/view/pages/pbxconfig/PbxConfig.js
@@ -5,6 +5,24 @@ Ext.define('NgcpCsc.view.pages.pbxconfig.PbxConfig', {
     viewModel: 'pbxconfig',
     controller: 'pbxconfig',
 
+    // What's done:
+    //  DONE: 1. Reorder menu to groups, seats, devices order
+    //  DONE: 2. Map out the data flow, see workfront docs
+    //  DONE: 3. Plan data store merge implementation
+    //  DONE: 4. Plan out what needs validation, and is required
+    //  DONE: 5. Implement validation in view and controller
+    //  DONE: 8. Implement store merge as per Cvenusinos suggestion
+    //
+    // What's remaining:
+    // TODO: 9. Fix bugs with "delayed" save, or "first save not working when
+    //       adding new seat or group"
+    // DONE: 8g. Filter out field arrays where fieldLabel is "User" or "Type".
+    //            See this todo also in PbxController.js
+    // TODO: 6. Bottom of card gets cut off in seats when creating new, might
+    //     also be happening in other modules. Investigate and fix
+    // TODO: 7. Fix bug in Groups module, where hunt 'for' label does not get
+    //     extra margin-top when saved with enter, only with button
+
     initComponent: function() {
 
         this.dockedItems = [{
