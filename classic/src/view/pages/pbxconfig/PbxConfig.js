937d7658ca532770395e569e1768413e0e7acb9b
Robert Axelsen <raxelsen@sipwise.com>
2017-04-24 17:15:05 +0200

    TT#14851 Csc pbx consolidation, fixed, validation
    Done:
     1. Reorder menu to groups, seats, devices order
     2. Map out the data flow, see below and workfront docs:
    
    Remaining:
     3. Set up a plan for data store merge implementation, aiming for
        ability to either link tables in different stores, or put all in one
        store with the ability to just change the store in the views and for
        example prepend parent store to existing record references
     4. Plan out what needs validation, and is required. Use ExtJS
        specific validation to minimize code and complexity of implementation
     5. Bottom of card gets cut off in seats when creating new, might
        also be happening in other modules. Investigate and fix
    
    Change-Id: I2af41a6e9799b7cbd268b1217ed49b9381272d6a

--- classic/src/view/pages/pbxconfig/PbxConfig.js
+++ classic/src/view/pages/pbxconfig/PbxConfig.js
@@ -5,6 +5,74 @@ Ext.define('NgcpCsc.view.pages.pbxconfig.PbxConfig', {
     viewModel: 'pbxconfig',
     controller: 'pbxconfig',
 
+    // DONE: 0. Create a todo list for all subtasks
+
+    // DONE: 1. Reorder menu to groups, seats, devices order
+    // DONE: 2. Map out the data flow, see below and workfront docs:
+
+    //                            ________________
+    //                            \               \                 .--------------.
+    //                             \               \                |    Groups    |
+    //      .-------------------->  ) Add New Group ) ------------> | D            |
+    //      |                      /               /                |              |
+    //      |                     /_______________/                 '--------------'
+    //      |                                                               |
+    //      |                             | .-------------------------------'
+    //                                    | |
+    // ***********                        v v
+    // *    o    *                _______________
+    // *   /|\   *                \              \                  .--------------.
+    // *   / \   *                 \              \  -------------> |    Seats     |
+    // *         *                  ) Add New Seat )                | D            |
+    // ***********                 /              /                 |              |
+    //                            /______________/                  '--------------'
+    //                                                                      |
+    //                                    | .-------------------------------'
+    //                                    | |
+    //                                    v v
+    //                            _________________
+    //                            \                \                .--------------.
+    //                             \                \               |   Devices    |
+    //                              ) Add New Device ) -----------> | D            |
+    //                             /                /               |              |
+    //                            /________________/                '--------------'
+
+    //                            ________________
+    //                            \               \                 .--------------.
+    //                             \               \  <------------ |    Groups    |
+    //      .-------------------->  ) View Groups   )               | D            |
+    //      |                      /               /                |              |
+    //      |                     /_______________/                 '--------------'
+    //      |                                                               |
+    //      |                               .-------------------------------'
+    //                                      |
+    // ***********                          v
+    // *    o    *                _______________
+    // *   /|\   *                \              \                  .--------------.
+    // *   / \   *                 \              \  <------------- |    Seats     |
+    // *         *                  ) View Seats   )                | D            |
+    // ***********                 /              /                 |              |
+    //                            /______________/                  '--------------'
+    //                                                                      |
+    //                                      .-------------------------------'
+    //                                      |
+    //                                      v
+    //                            _________________
+    //                            \                \                .--------------.
+    //                             \                \  <----------- |   Devices    |
+    //                              ) View Devices   )              | D            |
+    //                             /                /               |              |
+    //                            /________________/                '--------------'
+
+    // TODO: 3. Set up a plan for data store merge implementation, aiming for
+    // ability to either link tables in different stores, or put all in one
+    // store with the ability to just change the store in the views and for
+    // example prepend parent store to existing record references
+    // TODO: 4. Plan out what needs validation, and is required. Use ExtJS
+    // specific validation to minimize code and complexity of implementation
+    // TODO: 5. Bottom of card gets cut off in seats when creating new, might
+    // also be happening in other modules. Investigate and fix
+
     initComponent: function() {
 
         this.dockedItems = [{
