7def17c1e90b97948380dada61c139d82903bf25
Markus Danek <mdanek@sipwise.com>
2017-05-09 15:17:01 +0200

    TT#15802 Run 'npm install' in Dockerfile instead in testrunner
    Change-Id: Iaf22d3c31978567008f7e996208fa2246827dd4f

--- t/specs/wdio-setup.spec.js
+++ t/specs/wdio-setup.spec.js
@@ -3,6 +3,6 @@ describe('webdriver.io page', function() {
     it('should have the right title - the fancy generator way', function () {
         browser.url('http://localhost:1841');
         var title = browser.getTitle();
-        assert.equal(title, 'NgcpCsc Dashboard');
+        assert.equal(title, 'Sipwise Customer Portal');
     });
 });
