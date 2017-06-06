7247692f85f5789adbecd5a366ac817bfb46c729
Robert Axelsen <raxelsen@sipwise.com>
2017-06-06 17:01:45 +0200

    TT#17023 Csc API connection for CF module
    What is remaining:
     1. identify in API the endpoints required to perform crud operations
        for each of the submodules
     2. create proxy configurations for each store/models, extending
        NgcpApi proxy. Wait for callblocking API task to be completed
        first
    
    Change-Id: I7248d682dcb58cb4c23d449c31f2ea45c91f4e42

--- classic/src/view/pages/callforward/CallForward.js
+++ classic/src/view/pages/callforward/CallForward.js
@@ -1,3 +1,5 @@
+// TODO: 1. identify in API the endpoints required to perform crud operations for each of the submodules
+// TODO: 2. create proxy configurations for each store/models, extending NgcpApi proxy - pls wait for callblocking API task to be completed.
 Ext.define('NgcpCsc.view.pages.callforward.CallForward', {
     extend: 'Ext.panel.Panel',
 
