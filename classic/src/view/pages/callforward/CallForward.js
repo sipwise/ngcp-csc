deccf0d6f50883aef66e805f0cba4126b9a1c97a
Robert Axelsen <raxelsen@sipwise.com>
2017-06-06 17:01:45 +0200

    TT#17023 Csc API connection for CF module
    What is remaining:
     1. identify in API the endpoints required to perform crud operations
        for each of the submodules
     1b. verify endpoints with use of curl
     2. create proxy configurations for each store/models, extending
        NgcpApi proxy. Wait for callblocking API task to be completed
        first
    
    What has been done:
     1a. draw out all submodule endpoints
    
    Change-Id: I7248d682dcb58cb4c23d449c31f2ea45c91f4e42

--- classic/src/view/pages/callforward/CallForward.js
+++ classic/src/view/pages/callforward/CallForward.js
@@ -1,3 +1,26 @@
+// TODO: 1. identify in API the endpoints required to perform crud
+//          operations for each of the submodules
+// DONE: 1a. draw out all submodule endpoints, not taking into
+//           account query combinations (List A, Everyone, etc)
+// |   Submodule   |   Grid    |     CRUD     |      Endpoint      |
+// |---------------|-----------|--------------|--------------------|
+// | Always        | Timeset   | Read, Update | /api/cf*/          |
+// | Always        | Sourceset | Read, Update | /api/cf*/          |
+// | Always        | Online    | Read, Update | /api/callforwards/ |
+// | Always        | Busy      | Read, Update | /api/callforwards/ |
+// | Always        | Offline   | Read, Update | /api/callforwards/ |
+// | After Hours   | Timeset   | Read, Update | /api/cf*/          |
+// | After Hours   | Sourceset | Read, Update | /api/cf*/          |
+// | After Hours   | Online    | Read, Update | /api/callforwards/ |
+// | After Hours   | Busy      | Read, Update | /api/callforwards/ |
+// | After Hours   | Offline   | Read, Update | /api/callforwards/ |
+// | Company Hours | Timeset   | Read, Update | /api/cf*/          |
+// | Company Hours | Sourceset | Read, Update | /api/cf*/          |
+// | Company Hours | Online    | Read, Update | /api/callforwards/ |
+// | Company Hours | Busy      | Read, Update | /api/callforwards/ |
+// | Company Hours | Offline   | Read, Update | /api/callforwards/ |
+// TODO: 1b. verify endpoints with use of curl
+// TODO: 2. create proxy configurations for each store/models, extending NgcpApi proxy - pls wait for callblocking API task to be completed.
 Ext.define('NgcpCsc.view.pages.callforward.CallForward', {
     extend: 'Ext.panel.Panel',
 
