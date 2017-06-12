348c4cfd3ab91b98f518ba3136171fa6d114db91
Robert Axelsen <raxelsen@sipwise.com>
2017-06-12 19:21:47 +0200

    TT#17451 Csc implement GET for callforward API
    What has been done:
     1. Update endpoints mapping, as callforwards has sources and times,
        but no sourceset or timeset names. Need to perform an ajax
        request in controller to get sourceset name for current cf type
        for subscriber
    +--------------+---------------+------+-------------------------+
    | Submodule    | Grid          | CRUD | Endpoint                |
    +--------------+---------------+------+-------------------------+
    | Alw/Aft/Comp | Timeset       | RU   | /api/cftimesets/        |
    | Alw/Aft/Comp | Sourceset     | RU   | /api/cfsourcesets/      |
    | Alw/Aft/Comp | Onl/Offl/Busy | CRUD | /api/cfmappings/        |
    | Alw/Aft/Comp | Onl/Offl/Busy | CR   | /api/cfsourcesets/      |
    | Alw/Aft/Comp | Onl/Offl/Busy | CR   | /api/cftimesets/        |
    | Alw/Aft/Comp | Onl/Offl/Busy | CRUD | /api/cfdestinationsets/ |
    +--------------+---------------+------+-------------------------+
    (timeset/sourceset in onl/off/busy CRUD depends on UI implementation)
     2. Implement main store, proxy and controller for destination grids
     3a. implement into view for displaying data based on requested data
     3b. BUG fix. Duplicate destinations when loading several submodules
     4. CANCEL button text does not get reverted back to ADD NEW
        DESTINATION after adding new from empty list, fix
     5. Make timeout field editable
     6. For busy and offline, don't display "first ring" section
     7a. Read and display sourceset and timeset API data
     7b. BUG fix. List A grid is not showing
     8. Fix "Voicemail" rendering
     9. BUG fix. emptyText not showing for CallForwardMainGrid.js grids
     10. Added box-shadow for core-container and cards
    
    Change-Id: I1112969673296150e20a3add427fce81473d7060

--- app/store/CallForwardTimeset.js
+++ app/store/CallForwardTimeset.js
@@ -5,6 +5,22 @@ Ext.define('NgcpCsc.store.CallForwardTimeset', {
 
     model: 'NgcpCsc.model.CallForwardTimeset',
 
-    autoLoad: true
+    autoLoad: true,
+
+    proxy: {
+        type: 'ngcp-api',
+        route: 'cftimesets',
+        params: 'subscriber_id=' + localStorage.getItem('subscriber_id'),
+        actionMethods: {
+            read: 'GET',
+            update: 'PATCH'
+        }
+    },
+
+    listeners: {
+        load: function(store, recs) {
+            this.fireEvent('cfTimesetStoreLoaded', this, recs[0].data._embedded['ngcp:cftimesets']);
+        }
+    }
 
 });
