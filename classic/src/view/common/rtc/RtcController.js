7317e6e9dd7b591bca7be351af7f450981bd70d1
Robert Axelsen <raxelsen@sipwise.com>
2017-08-25 16:17:48 +0200

    TT#21017 Create util to play/stop sound
    Change-Id: Ia2064f9f28d6124596012dc4c17b027c6115ff6f

--- classic/src/view/common/rtc/RtcController.js
+++ classic/src/view/common/rtc/RtcController.js
@@ -214,11 +214,10 @@ Ext.define('NgcpCsc.view.common.rtc.RtcController', {
             clearInterval(me.intervalId);
         }
         vm.set('status', 'calling...');
-        sample.play();
+        me.playRingSound();
         setTimeout(function() {
             var seconds = minutes = hours = 0;
-            sample.pause();
-            sample.currentTime = 0;
+            me.stopRingSound();
             vm.set('callEnabled', true);
             vm.set('micEnabled', true);
 
@@ -519,6 +518,19 @@ Ext.define('NgcpCsc.view.common.rtc.RtcController', {
     setRtcpanelTitleColor: function (state) {
         var rtcpanel = Ext.getCmp('rtcpanel');
         rtcpanel.toggleCls('rtc-title-call-initiation', state);
+    },
+
+    playRingSound: function () {
+        var sound = document.getElementById('ring');
+        if (sound.paused) {
+            sound.play();
+        };
+    },
+
+    stopRingSound: function () {
+        var sound = document.getElementById('ring');
+        sound.pause();
+        sound.currentTime = 0;
     }
 
 });
