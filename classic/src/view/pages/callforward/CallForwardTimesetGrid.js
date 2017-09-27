e7bd28ffbb9686b3853f1f70b207813c56da695c
Carlo <cvenusino@sipwise.com>
2017-09-27 14:04:03 +0200

    TT#21846 NGCP-CSC CallForwarding Timeset remove period column
    Change-Id: Id0174516a2014c6079047a7155501e694e436172

--- classic/src/view/pages/callforward/CallForwardTimesetGrid.js
+++ classic/src/view/pages/callforward/CallForwardTimesetGrid.js
@@ -25,6 +25,7 @@ Ext.define('NgcpCsc.view.pages.callforward.CallForwardTimesetGrid', {
                 flex: 1,
                 widget: {
                     xtype: 'timefield',
+                    margin: '8 0 0 0',
                     tooltip: Ngcp.csc.locales.callforward.tooltips.change_time_from[localStorage.getItem('languageSelected')],
                     bind: {
                         value: '{record.time_from}'
@@ -38,11 +39,20 @@ Ext.define('NgcpCsc.view.pages.callforward.CallForwardTimesetGrid', {
                 flex: 1,
                 widget: {
                     xtype: 'timefield',
+                    margin: '8 0 0 0',
                     tooltip: Ngcp.csc.locales.callforward.tooltips.change_time_to[localStorage.getItem('languageSelected')],
                     bind: {
                         value: '{record.time_to}'
                     }
                 }
+            }, {
+                xtype: 'actioncolumn',
+                width: 30,
+                items: [{
+                    iconCls: Ngcp.csc.icons.trash,
+                    tooltip:  Ngcp.csc.locales.filters.tooltips.remove_entry[localStorage.getItem('languageSelected')],
+                    handler: 'removePeriod'
+                }]
             }]
         };
         this.callParent();
